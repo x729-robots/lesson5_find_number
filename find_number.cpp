242d687d519cad3b6dd1b38b5968cd0b4f673ac9
developer <x729-robots@yandex.ru>
2024-05-03 13:38:48 +0300

    find_number.cpp_finished

--- find_number.cpp
+++ find_number.cpp
@@ -8,20 +8,20 @@ int main(int argc, char** argv)
 {
 
 	int maxValue(0);
-	if(readArgs(int argc, char** argv, int &maxValue)){return 1;}
+	if(readArgs(argc, const_cast<const char**>(argv), maxValue)){return 1;}
 	
 	int randomNumber(0);
-	if(calcRandomNumber(int maxValue,int &randomNumber)){return 1;}
+	if(calcRandomNumber(maxValue, randomNumber)){return 1;}
 	
 	std::string user;
 	user = getUser();
 	
 	int attemptsN;
-	if(superGame(randomNumber, int &attemptsN)){return 1};
+	if(superGame(randomNumber, attemptsN)){return 1;}
 	
 	const std::string logFName("find_number.log");
 	if(resultToLog(logFName,user, attemptsN)){return 1;}
-	if(printLog(logFName)){return 1};
+	if(printLog(logFName)){return 1;}
 
 	return 0;
 }
